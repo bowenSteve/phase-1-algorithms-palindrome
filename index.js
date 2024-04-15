6a93a7c066f59d0744818f10c5df36d60fa80fbb
Stephen Bowen <bowensteven17@gmail.com>
2024-04-15 09:57:06 +0300

    complete algorithms palindrome lab

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  const lowerCasedWord=word.toLowerCase()
+
+  const splitWord=lowerCasedWord.split('');
+
+  const reversedWord= splitWord.reverse().join('')
+
+    if (word === reversedWord) {
+       return true;
+    } else {
+      return false;
+  }
+
 }
 
-/* 
-  Add your pseudocode here
+/*
+Pseudocode:  
+convert word to lowercase
+reverse the lowercase version of the word using split, reverse and join
+compare this reversed word and the input word, if they are equal, return true
+if they are not equal return false
 */
 
 /*
-  Add written explanation of your solution here
+First, the case of the input is changed to lowercase. Then, the word is split using the split method. 
+After splitting, the reverse method is performed on the word, which reverses it. Subsequently, the join 
+method is applied to the word to create a reversed word. Finally, the reversed word is compared with the 
+input word. This comparison returns true if the two are equal and false if they are not equal.
 */
 
 // You can run `node index.js` to view these console logs
